5455ec19007496100afbbe3eb2297cdb44250c1a
Radoslaw Mulawka <radekmajestic@gmail.com>
2017-10-07 22:51:33 +0200

    Created script for pop up block, created it so, that you cant open new window if there is already opened another one

--- script/script.js
+++ script/script.js
@@ -6,32 +6,107 @@ $(window).bind('scroll', function () {
     }
 });
 
-$(".service-item").eq(0).on("click",function(){
-	$(".hidden-service-block").eq(0).fadeIn(500);
+
+
+
+
+
+
+
+
+
+
+$(".service-item").eq(0).find('img').on("click",function(){
+	if($(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
+		 $(".hidden-service-block").eq(0).fadeIn(500);
+	}
+	else {
+		
+	}
+	
 });
-$(".service-item").eq(1).on("click",function(){
-	$(".hidden-service-block").eq(1).fadeIn(500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(".service-item").eq(1).find('img').on("click",function(){
+	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
+		 $(".hidden-service-block").eq(1).fadeIn(500);
+	}
+	else {
+		
+	}
 });
-$(".service-item").eq(2).on("click",function(){
-	$(".hidden-service-block").eq(2).fadeIn(500);
+$(".service-item").eq(2).find('img').on("click",function(){
+	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
+		 $(".hidden-service-block").eq(2).fadeIn(500);
+	}
+	else {
+		
+	}
 });
-$(".service-item").eq(3).on("click",function(){
-	$(".hidden-service-block").eq(3).fadeIn(500);
+$(".service-item").eq(3).find('img').on("click",function(){
+	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
+		 $(".hidden-service-block").eq(3).fadeIn(500);
+	}
+	else {
+		
+	}
 });
-$(".service-item").eq(4).on("click",function(){
-	$(".hidden-service-block").eq(4).fadeIn(500);
+$(".service-item").eq(4).find('img').on("click",function(){
+	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
+		 $(".hidden-service-block").eq(4).fadeIn(500);
+	}
+	else {
+		
+	}
 });
-$(".service-item").eq(5).on("click",function(){
-	$(".hidden-service-block").eq(5).fadeIn(500);
+$(".service-item").eq(5).find('img').on("click",function(){
+	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
+		 $(".hidden-service-block").eq(5).fadeIn(500);
+	}
+	else {
+		
+	}
 });
-$(".service-item").eq(6).on("click",function(){
-	$(".hidden-service-block").eq(6).fadeIn(500);
+$(".service-item").eq(6).find('img').on("click",function(){
+	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
+		 $(".hidden-service-block").eq(6).fadeIn(500);
+	}
+	else {
+		
+	}
 });
-$(".service-item").eq(7).on("click",function(){
-	$(".hidden-service-block").eq(7).fadeIn(500);
+$(".service-item").eq(7).find('img').on("click",function(){
+	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
+		 $(".hidden-service-block").eq(7).fadeIn(500);
+	}
+	else {
+		
+	}
 });
-$(".service-item").eq(8).on("click",function(){
-	$(".hidden-service-block").eq(8).fadeIn(500);
+$(".service-item").eq(8).find('img').on("click",function(){
+	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none'){
+		 $(".hidden-service-block").eq(8).fadeIn(500);
+	}
+	else {
+		
+	}
 });
 
 
