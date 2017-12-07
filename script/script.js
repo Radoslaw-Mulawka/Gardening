6f6c83a60ddbd75bd8c23de11458eda6b812650c
Radoslaw Mulawka <radekmajestic@gmail.com>
2017-12-07 15:07:26 +0100

    Decided to remaster full site, here is a new version's first commit

--- script/script.js
+++ script/script.js
@@ -1,109 +1,40 @@
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > 20) {
-        $('.top-menu').addClass('fixed');
-    } else {
-        $('.top-menu').removeClass('fixed');
-    }
-});
-
-
-$(".service-item").eq(0).find('img, p').on("click",function(){
-	if($(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
-		 $(".hidden-service-block").eq(0).fadeIn(500);
-	}
-	else {
-		
-	}
-	
-});
-$(".service-item").eq(1).find('img, p').on("click",function(){
-	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
-		 $(".hidden-service-block").eq(1).fadeIn(500);
-	}
-	else {
-		
-	}
-});
-$(".service-item").eq(2).find('img, p').on("click",function(){
-	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
-		 $(".hidden-service-block").eq(2).fadeIn(500);
-	}
-	else {
-		
-	}
-});
-$(".service-item").eq(3).find('img, p').on("click",function(){
-	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
-		 $(".hidden-service-block").eq(3).fadeIn(500);
-	}
-	else {
-		
-	}
-});
-$(".service-item").eq(4).find('img, p').on("click",function(){
-	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
-		 $(".hidden-service-block").eq(4).fadeIn(500);
-	}
-	else {
-		
-	}
-});
-$(".service-item").eq(5).find('img, p').on("click",function(){
-	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
-		 $(".hidden-service-block").eq(5).fadeIn(500);
-	}
-	else {
-		
-	}
-});
-$(".service-item").eq(6).find('img, p').on("click",function(){
-	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
-		 $(".hidden-service-block").eq(6).fadeIn(500);
-	}
-	else {
-		
-	}
-});
-$(".service-item").eq(7).find('img, p').on("click",function(){
-	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(8).css('display') == 'none'){
-		 $(".hidden-service-block").eq(7).fadeIn(500);
-	}
-	else {
-		
-	}
-});
-$(".service-item").eq(8).find('img, p').on("click",function(){
-	if($(".hidden-service-block").eq(0).css('display') == 'none' && $(".hidden-service-block").eq(1).css('display') == 'none' && $(".hidden-service-block").eq(2).css('display') == 'none' && $(".hidden-service-block").eq(3).css('display') == 'none' && $(".hidden-service-block").eq(4).css('display') == 'none' && $(".hidden-service-block").eq(5).css('display') == 'none' && $(".hidden-service-block").eq(6).css('display') == 'none' && $(".hidden-service-block").eq(7).css('display') == 'none'){
-		 $(".hidden-service-block").eq(8).fadeIn(500);
-	}
-	else {
-		
-	}
-});
-
-
-
-
-$(".close-button").click(function(){
-	$(this).parent().fadeOut(300);
-});
-
-
 $(document).ready(function() {
   //carousel options
   $('#quote-carousel').carousel({
     pause: true, interval: 10000,
   });
+
+
+
+  $("#lightgallery").lightGallery(); 
+
+
+
+
+  	$(".hamburger").on("click", function(){
+		if($(".hidden-nav").css("display") == "none") {
+			$(".hidden-nav").slideDown();
+		}
+		else {
+			$(".hidden-nav").slideUp();
+		}
+
+	});
+
+
+
+
+
+
+
+  	
+
+
+
+
+
+
+
+
 });
-
-
-$(document).ready(function() {
-     $("#lightgallery").lightGallery(); 
-});
-
-$('.videobackground p').hide().fadeIn(3500);
-
-
-
-// service section shadow animation 
 
