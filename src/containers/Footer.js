892ab5ca9d349208f163334ba0dba4b54e767589
Radoslaw Mulawka <radekmajestic@gmail.com>
2018-04-13 16:34:00 +0200

    deleted many redundant files and images. Changes totally image gallery - now using lightwidget for instagramm. Made footer partners a link

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -2,41 +2,49 @@ import React from 'react';
 import GlobalContainer from './GlobalContainer';
 
 
-const Footer = (props)=>{
- return(
- 		<GlobalContainer>
- 			<footer>
-	            <div className="adress">
-	                <strong>Usługi:</strong>
-	                <div>
-	                  <div>Pięlegnacja zieleni</div>
-	                  <div>Zakładanie ogrodów</div>
-	                  <div>Usługi ogrodnicze Wrocław i okolice</div>
-	                </div>
-	            </div>
-	            <div className="partners">
-	                <strong>Partnerzy:</strong>
-	                <div className="partners-wrap">
-	                    <div className="partner-logo">Logo</div>
-	                    <div className="partner-logo">Logo</div>
-	                    <div className="partner-logo">Logo</div>
-	                    <div className="partner-logo">Logo</div>
-	                </div> 
-	            </div>
-	            <div className="footer-contacts">
-	                <strong>Contacts:</strong> 
-	                <div>
-	                  <div>tel: +48 731 924 406</div>
-	                  <div className="footer-email">email: uslugi@greenpolis.pl </div>
-	                  <div className="social-media">
-	                      <a href="http://www.facebook.com"><i className="fab fa-facebook-f" aria-hidden="true"></i></a> 
-	                      <a href="http://www.instagram.com"><i className="fab fa-instagram"></i></a>
-	                  </div>            
-	                </div>           
-	            </div>
-	        </footer>
- 		</GlobalContainer>
- 	)
+const Footer = (props) => {
+	return (
+		<GlobalContainer>
+			<footer>
+				<div className="adress">
+					<strong>Usługi:</strong>
+					<div>
+						<div>Pięlegnacja zieleni</div>
+						<div>Zakładanie ogrodów</div>
+						<div>Usługi ogrodnicze Wrocław i okolice</div>
+					</div>
+				</div>
+				<div className="partners">
+					<strong>Partnerzy:</strong>
+					<div className="partners-wrap">
+						<a href="" className="partner-logo">
+							<div ></div>
+						</a>
+						<a href="" className="partner-logo">
+							<div >Logo</div>
+						</a>
+						<a href="" className="partner-logo">
+							<div >Logo</div>
+						</a>
+						<a href="" className="partner-logo">
+							<div >Logo</div>
+						</a>
+					</div>
+				</div>
+				<div className="footer-contacts">
+					<strong>Contacts:</strong>
+					<div>
+						<div>tel: +48 731 924 406</div>
+						<div className="footer-email">email: uslugi@greenpolis.pl </div>
+						<div className="social-media">
+							<a href="http://www.facebook.com"><i className="fab fa-facebook-f" aria-hidden="true"></i></a>
+							<a href="http://www.instagram.com"><i className="fab fa-instagram"></i></a>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</GlobalContainer>
+	)
 }
 
 
